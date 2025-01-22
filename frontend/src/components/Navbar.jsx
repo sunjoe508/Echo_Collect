278e7aae97b5ee3d202f6932c23447fcfe1c660e
Joemunga <133594633+sunjoe508@users.noreply.github.com>
2025-01-22 22:28:55 +0300

    Create Navbar.jsx

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Navbar() {
+  return (
+    <nav>
+      <h1>EcoCollect</h1>
+      <ul>
+        <li>Home</li>
+        <li>Dashboard</li>
+        <li>Schedule Pickup</li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
